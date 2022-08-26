8e0b3b763e75d334f0770ecf2c72959454698f74
merkukhinivan <ivanmerk03@gmail.com>
2022-08-26 12:12:57 +0300

    webpack 1.1

--- 6-css-architecture-and-preprocessors/Cootels/webpack.config.js
+++ 6-css-architecture-and-preprocessors/Cootels/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin'); 
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -14,7 +15,13 @@ module.exports = {
         new HTMLWebpackPlugin({
             template: './index.html'
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, 'src/assets/img'), 
+                to: path.resolve(__dirname, 'dist/img') },
+            ],
+        }),
     ],
     module: {
         rules:[
